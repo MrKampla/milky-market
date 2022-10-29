906a721d1796c1de8d507b0167caf6f55e705155
MrKampla <mrkampla@gmail.com>
2022-10-29 21:13:23 +0200

    Connect contracts repo with frontend and setup web3 wallet connection

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { publicProvider } from 'wagmi/providers/public';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+
+const { chains, provider } = configureChains(
+  [chain.localhost, chain.polygon, chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
+    publicProvider(),
+  ],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Milky Market',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
