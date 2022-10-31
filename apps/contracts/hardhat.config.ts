95a0d6d296e325d105b8ad287625f46b0103a570
MrKampla <mrkampla@gmail.com>
2022-10-31 01:29:20 +0100

    Add deployment script

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,24 +1,32 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import dotenv from "dotenv";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
   networks: {
+    localhost: {
+      forking: {
+        url: process.env.POLYGON_MAINNET_RPC!,
+        blockNumber: 34881158,
+      },
+      chainId: 31337,
+    },
     hardhat: {
       forking: {
         url: process.env.POLYGON_MAINNET_RPC!,
         blockNumber: 34881158,
       },
+      chainId: 31337,
       accounts: [
         {
           privateKey: process.env.PRIVATE_KEY!,
-          balance: "100315414324631460577",
+          balance: '100315414324631460577',
         },
         {
           privateKey: process.env.PRIVATE_KEY_2!,
-          balance: "100000000000000000000",
+          balance: '100000000000000000000',
         },
       ],
     },
