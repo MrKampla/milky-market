1424dc931fd586ec8b577ab0ca0e3ab5083c2b91
MrKampla <mrkampla@gmail.com>
2022-11-02 17:55:35 +0100

    Add fallback random private key to hardhat

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -3,6 +3,9 @@ import '@nomicfoundation/hardhat-toolbox';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const DEFAULT_RANDOM_PRIVATE_KEY =
+  '0xf4e8d0152e29b2741d26d1ed0c325b41e33fce30e6b2d4deae4a712afd322e37';
+
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   networks: {
@@ -21,11 +24,11 @@ const config: HardhatUserConfig = {
       chainId: 31337,
       accounts: [
         {
-          privateKey: process.env.PRIVATE_KEY ?? '',
+          privateKey: process.env.PRIVATE_KEY ?? DEFAULT_RANDOM_PRIVATE_KEY,
           balance: '100315414324631460577',
         },
         {
-          privateKey: process.env.PRIVATE_KEY_2 ?? '',
+          privateKey: process.env.PRIVATE_KEY_2 ?? DEFAULT_RANDOM_PRIVATE_KEY,
           balance: '100000000000000000000',
         },
       ],
@@ -33,12 +36,12 @@ const config: HardhatUserConfig = {
     polygon: {
       url: process.env.POLYGON_MAINNET_RPC!,
       chainId: 137,
-      accounts: [process.env.PRIVATE_KEY ?? ''],
+      accounts: [process.env.PRIVATE_KEY ?? DEFAULT_RANDOM_PRIVATE_KEY],
     },
     mumbai: {
       url: process.env.POLYGON_MUMBAI_RPC!,
       chainId: 80001,
-      accounts: [process.env.PRIVATE_KEY ?? ''],
+      accounts: [process.env.PRIVATE_KEY ?? DEFAULT_RANDOM_PRIVATE_KEY],
     },
   },
 };
